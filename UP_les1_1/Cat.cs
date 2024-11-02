de43cc98504e397152b762a45cc24c6b17822cd2
AgataSergeevaPR31 <agatasergeeva60@gmail.com>
2024-11-02 11:14:34 +0500

    Добавьте файлы проекта.

---
+++ UP_les1_1/Cat.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP_les1_1
+{
+    class Cat
+    {
+        //создаём поля класса
+        private string name;
+        private  double weight;
+        public string Name // свойство, реализуем инкапсуляцию!
+        {
+            // получение значения - просто возврат name
+            get
+            {
+                return name;
+            }
+            // установка значения - используем проверку
+            set
+            {
+                bool OnlyLetters = true;
+                // ключ. слово value - это то, что хотят свойству присвоить
+                foreach (var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+
+                if (OnlyLetters)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильное имя!!!");
+                    name = "Неизвестный";
+                }
+            }
+        }
+
+        public double Weight // свойство, реализуем инкапсуляцию!
+        {
+            // получение значения - просто возврат weight
+            get
+            {
+                return weight;
+            }
+            // установка значения - используем проверку
+            set
+            {
+                bool CorrectNumber = true;
+                // ключ. слово value - это то, что хотят свойству присвоить
+                //проверка на то, что ввёденное пользователем значение double и больше 0
+                if (value <= 0) CorrectNumber = false;
+
+                if (CorrectNumber)
+                {
+                    weight = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - такого веса не может быть!!! Мы заменили его на средний - 4\n");
+                    weight = 4;
+                }
+            }
+        }
+
+        //конструктор для создания нового экземпляра
+        public Cat(string CatName, double CatWeight)
+        {
+            Name = CatName;
+            Weight = CatWeight;
+        }
+
+        //метод, который бы позволял опознать отдельно взятого кота
+        public void Meow()
+        {
+            Console.WriteLine($"{name}: МЯЯЯЯУ!!!!\nВЕС: {weight}\n");
+        }
+    }
+}
---
+++ UP_les1_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP_les1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            double weight = 0;
+
+            while (true)
+            {
+                Console.Write("Введите имя коту: ");
+                name = Convert.ToString(Console.ReadLine());
+                //проверка на выход заполнения котов
+                if (name == "exit")
+                {
+                    break;
+                }
+
+                while (true)
+                {
+                    Console.Write("Введите вес кота: ");
+                    string weig = Convert.ToString(Console.ReadLine());
+
+                    if (double.TryParse(weig, out weight))
+                    {
+                        break;
+                    }
+                }
+
+                if (weight == -100)
+                {
+                    break;
+                }
+
+                //создаём объект, вызывая конструктор класса
+                Cat murzik = new Cat(name, weight);
+                //вызываем метод класса
+                murzik.Meow();
+            }
+            Console.Write("Заполнение закончено!");
+
+            Console.ReadKey();
+        }
+    }
+}
